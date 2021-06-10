22c632ba45202b41845c4eb31b8c35e776b44398
cristian mosquera <cmosqbone1@gmail.com>
2021-06-10 02:14:13 +0200

    add footer and contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from "react"
 import socialLinks from "../constants/social_links"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <div className="footer-links social-links">
+          {socialLinks.map(socialLink => (
+            <a
+              key={socialLink.id}
+              href={socialLink.url}
+              className="social-link"
+            >
+              {socialLink.icon}
+            </a>
+          ))}
+        </div>
+
+        <h4>
+          Copyright &copy; {new Date().getFullYear()}
+          <span>Cristian Mosquera</span>
+        </h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
