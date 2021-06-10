9999625ad33856d21d37db5ec8d5c56ceb84424f
cristian mosquera <cmosqbone1@gmail.com>
2021-06-10 03:10:58 +0200

    fixed links

--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -11,27 +11,27 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/cristian.mosquera.9279807",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/cristian.mosquera.9279807",
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/cristian.mosquera.9279807",
   },
   {
     id: 4,
     icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/cristian.mosquera.9279807",
   },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/cristian.mosquera.9279807",
   },
 ]
 
